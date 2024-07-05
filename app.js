e4ea928f2d2bfe7023398bbc466312070fbc5ad5
sainathreddy2003 <149447817+sainathreddy2003@users.noreply.github.com>
2024-07-05 11:17:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -84,6 +84,9 @@ app.use((req,res,next)=>{
 app.use("/listings",listingsRouter);
 app.use("/listings/:id/reviews",reviewsRouter);
 app.use("/",userRouter);
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use((err,req,res,next)=>{
   let {statusCode,message} =err;
